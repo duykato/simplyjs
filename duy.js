a19bf3f35770962a33c912506be6a5ab0fc4116c
duykato <leduyq@gmail.com>
2013-12-15 15:49:15 -0800

    Update duy.js

--- duy.js
+++ duy.js
@@ -14,20 +14,29 @@ var winLose = [
 
 //Math.floor (0.05 * 4) = 0
 
+/*
 var getRandomIndex = function() {
   var winLoseIndex = Math.floor(Math.random() * winLose.length);
 };
 
 var winLoseIndex = 0;
 
+var updateWinLose = function() {
+  simply.setText({body: winLose[winLoseIndex] });
+}
+
 simply.on('singleClick', function(e) {
   if (e.button === 'select'){
-    simply.setText({ title: winLose[getRandomIndex()] });
+    //Do Nothing
   } else if (e.button === 'up'){
     winLoseIndex = winLoseIndex - 1;
+    if (winLose < 0){winLoseIndex = winLoseIndex.[]};
+    updateWinLose();
   } else if {e.button === 'down'
     winLoseIndex = winLoseIndex + 1;
+    updateWinLose();
   }
 });
+*/
 
 simply.begin();
