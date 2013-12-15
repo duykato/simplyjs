6008cdbd3a01026bfe3627ec39fce52efb35c1a4
duykato <leduyq@gmail.com>
2013-12-15 15:58:47 -0800

    Update duy.js

--- duy.js
+++ duy.js
@@ -4,45 +4,5 @@ simply.setText({
   body: 'Heimerdinger'
 }, true);
 
-// a-z 0-9 _
-var winLose = [
-  {title: 'Victory'}, // 0
-  {title: 'Defeat'},  // 1
-  [title: 'NORMAL', body: 'Stats aren\'t tracked for normal games.']
-];
-
-simply.on('singleClick', function(e){
-  if (e.button === 'select')
-//do nothing
-});
-
-
-
-//Math.floor (0.05 * 4) = 0
-
-/*
-var getRandomIndex = function() {
-  var winLoseIndex = Math.floor(Math.random() * winLose.length);
-};
-
-var winLoseIndex = 0;
-
-var updateWinLose = function() {
-  simply.setText({body: winLose[winLoseIndex] });
-}
-
-simply.on('singleClick', function(e) {
-  if (e.button === 'select'){
-    //Do Nothing
-  } else if (e.button === 'up'){
-    winLoseIndex = winLoseIndex - 1;
-    if (winLose < 0){winLoseIndex = winLoseIndex.[]};
-    updateWinLose();
-  } else if {e.button === 'down'
-    winLoseIndex = winLoseIndex + 1;
-    updateWinLose();
-  }
-});
-*/
 
 simply.begin();
