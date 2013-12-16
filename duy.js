e1758798b2e33b2bf38311a9b9b84ec81546cc0f
duykato <leduyq@gmail.com>
2013-12-15 16:04:21 -0800

    Update duy.js

--- duy.js
+++ duy.js
@@ -4,5 +4,18 @@ simply.setText({
   body: 'Heimerdinger'
 }, true);
 
+var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?q=London, uk';
+
+var requestWeather = function(){
+  ajax({ url: weatherUrl, type: 'json'}, function(data) {
+    simply.setText({ subtitle: data.main.temp});
+  });
+};
+
+simply.on('singleClick', function(e){
+  if(e.button=='select'){
+    requestWeather();
+  }
+})
 
 simply.begin();
