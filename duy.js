e3614934d6019b1d34355e3fd0100c3531566ad7
duykato <leduyq@gmail.com>
2013-12-15 18:30:08 -0800

    Update duy.js

--- duy.js
+++ duy.js
@@ -1,13 +1,28 @@
+var players = [
+  'DuyKato',
+  'timolawl',
+  'neoarchangel9',
+  'lifeisgood'
+];
+
 var lolApi = 'https://prod.api.pvp.net/api/lol/na/v1.1/summoner/by-name/duykato?api_key=1e0b2bdd-8bf5-43ba-8900-e7c606344517';
+
+
 
 var requestName = function() {
   ajax({ url: lolApi, type: 'json' }, function(data) {
     simply.setText({ title: data.name.toString() });
-    simply.settext({ subtitle: data.summonerLevel() });
+
     simply.settext({ body: 'Will this work?'})
   });
 };
 
+
+var requestSummonerLevel = function (){
+    ajax({ url: lolApi, type: 'json' }, function(data) {
+    simply.settext({ subtitle: data.summonerLevel() });      
+  });
+}
 requestName();
 
 
