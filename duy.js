cf4d71c7353da2a3d5b26b76cac3efdf351eecdb
duykato <leduyq@gmail.com>
2013-12-15 16:43:08 -0800

    Update duy.js

--- duy.js
+++ duy.js
@@ -4,16 +4,35 @@ simply.setText({
   body: "Heimerdinger"
 });
 
-var winLose [
-  'Victory',
-  'Defeat',
+var map = [
+  ['x','x','x','x','z','x',],
+  ['x','a','x','x','x','g',],
+  ['x','x','b','x','x','x',],
+  ['x','x','x','x','C','x',],
+  ['x','x','x','x','x','d',],
 ];
 
+var pos = { x: 2, y: 2};
+
+var dirs = [
+  { x: , y: };
+  { x: , y: };
+  { x: , y: };{ x: , y: };
+];
+  
+var dir = 0;
+
 simply.on('singleClick', function(e){
-  if (e.button == 'select'){
-    simply.setText({
-      title: winLose[1];
-    });
+  switch (e.button){
+    case 'select':
+      console.log('1');
+      break;
+    case 'up'::
+      console.log('2');
+      break;
+    case 'down':
+      console.log('3');
+      break;
   }
 });
 
