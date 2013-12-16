ac5036dfb8b2308ed49bc3a3e89d5ba4af1e864e
duykato <leduyq@gmail.com>
2013-12-15 19:08:24 -0800

    Update duy.js

--- duy.js
+++ duy.js
@@ -27,7 +27,7 @@ var initPlayers = function() {
       summary: { losses: 0, wins: 0 },
     };
   }
-}
+};
 
 var lolApiUrl = 'https://prod.api.pvp.net/api/lol/na/v1.1/';
 var lolApiKey = 'api_key=1e0b2bdd-8bf5-43ba-8900-e7c606344517';
@@ -55,6 +55,7 @@ var requestGameSummary = function(player) {
   });
 };
 
+initPlayers();
 requestSummonerId(lolDuy.players[lolDuy.playerIndex], requestGameSummary);
 
 simply.begin();
