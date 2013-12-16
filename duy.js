4ab1f9e6aec341cdec83394127315684cec98835
duykato <leduyq@gmail.com>
2013-12-15 18:18:15 -0800

    Update duy.js

--- duy.js
+++ duy.js
@@ -1,7 +1,7 @@
 var lolApi = 'https://prod.api.pvp.net/api/lol/na/v1.1/summoner/by-name/duykato?api_key=1e0b2bdd-8bf5-43ba-8900-e7c606344517';
 
 var requestName = function() {
-  ajax({ url: lolApi, type: 'json' }, function(name) {
+  ajax({ url: lolApi, type: 'json' }, function(data) {
     simply.setText({ subtitle: data.name.toString() });
   });
 };
