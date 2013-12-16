1d936e79fd9b1087399e0b8c7f0ef6ae81140979
duykato <leduyq@gmail.com>
2013-12-15 16:52:32 -0800

    Update duy.js

--- duy.js
+++ duy.js
@@ -4,35 +4,11 @@ simply.setText({
   body: "Heimerdinger"
 });
 
-var map = [
-  ['x','x','x','x','z','x',],
-  ['x','a','x','x','x','g',],
-  ['x','x','b','x','x','x',],
-  ['x','x','x','x','C','x',],
-  ['x','x','x','x','x','d',],
-];
-
-var pos = { x: 2, y: 2};
-
-var dirs = [
-  { x: , y: };
-  { x: , y: };
-  { x: , y: };{ x: , y: };
-];
-  
-var dir = 0;
-
 simply.on('singleClick', function(e){
-  switch (e.button){
-    case 'select':
-      console.log('1');
-      break;
-    case 'up'::
-      console.log('2');
-      break;
-    case 'down':
-      console.log('3');
-      break;
+  if (e.button == 'select'){
+    simply.setText({
+      title: 'Victory'
+    })
   }
 });
 
