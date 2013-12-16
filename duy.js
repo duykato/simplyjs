79604dd27a9086bfc078d359e36d16cac018dd47
duykato <leduyq@gmail.com>
2013-12-15 17:02:07 -0800

    Update duy.js

--- duy.js
+++ duy.js
@@ -4,16 +4,20 @@ simply.setText({
   body: "Heimerdinger"
 });
 
+
+
 var winLose = [
   'Victory',
   'Defeat'
 ];
 
 
+
 simply.on('singleClick', function(e){
+  var winLoseIndex = Math.floor(Math.random() * winLose.length);
   if (e.button == 'select'){
     simply.setText({
-      title: winLose[0]
+      title: winLose[winLoseIndex]
     })
   }
 });
