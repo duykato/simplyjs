2ee34bbf3990f2c5ce7dc1fccf1b3017c61fb4bb
duykato <leduyq@gmail.com>
2013-12-15 19:20:20 -0800

    Update duy.js

--- duy.js
+++ duy.js
@@ -49,6 +49,30 @@ var requestGameSummary = function(player) {
 };
 
 initPlayers();
-requestSummonerId(lolDuy.players[lolDuy.playerIndex], requestGameSummary);
+
+var updatePlayer = function() {
+  requestSummonerId(lolDuy.players[lolDuy.playerIndex], requestGameSummary);
+};
+
+var changePlayer = function(delta) {
+  lolDuy.playerIndex += delta;
+  if (lolDuy.playerIndex < 0) {
+    lolDuy.playerIndex = lolDuy.players.length - 1;
+  } else if (lolDuy.playerIndex >= lolDuy.players.length) {
+    lolDuy.playerIndex = 0;
+  } 
+};
+
+simply.on('singleClick', function(e) {
+  if (e.button === 'up') {
+    changePlayer(-1);
+    updatePlayer();
+  } else if (e.button === 'down') {
+    changePlayer(1);
+    updatePlayer();
+  }
+});
+
+updatePlayer();
 
 simply.begin();
