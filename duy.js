689e574c26c1e92bb73e5670ef8ecbf5cb68b82f
duykato <leduyq@gmail.com>
2013-12-15 18:13:57 -0800

    Update duy.js

--- duy.js
+++ duy.js
@@ -1,21 +1,16 @@
-simply.setText({
-  title: 'Result?',
-  subtitle: '30/2/3',
-  body: "Heimerdinger"
-});
+var lolApi = 'https://prod.api.pvp.net/api/lol/na/v1.1/summoner/by-name/duykato?api_key=1e0b2bdd-8bf5-43ba-8900-e7c606344517';
 
-var winLose = [
-  'Victory',
-  'Defeat'
-];
+var requestName = function() {
+  ajax({ url: lolApi, type: 'json' }, function(name) {
+    simply.setText({ subtitle: data.name.toString() });
+  });
+};
 
 simply.on('singleClick', function(e) {
   if (e.button == 'select') {
-    var winLoseIndex = Math.floor(Math.random() * winLose.length);
-    simply.setText({
-      title: winLose[winLoseIndex]
-    });
+    requestName();
   }
 });
+
 
 simply.begin();
