285b6339ac8ed50416a82c30f7cbdbb751efa71d
duykato <leduyq@gmail.com>
2013-12-15 19:13:50 -0800

    Update duy.js

--- duy.js
+++ duy.js
@@ -33,10 +33,10 @@ var lolApiUrl = 'https://prod.api.pvp.net/api/lol/na/v1.1';
 var lolApiKey = 'api_key=1e0b2bdd-8bf5-43ba-8900-e7c606344517';
 
 var requestSummonerId = function(player, callback) {
+  simply.setText({ title: player.name });
   var url = lolApiUrl+'/summoner/by-name/'+player.name+'?'+lolApiKey;
   ajax({ url: url, type: 'json' }, function(data) {
     player.id = data.id;
-    simply.setText({ title: player.id.toString() });
     if (callback) { callback(player); }
   });
 };
