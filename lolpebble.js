614951264696fe7420ab4b2bf41ca2fdd6dba270
duykato <leduyq@gmail.com>
2013-12-30 00:32:13 -0800

    Update lolpebble.js

--- lolpebble.js
+++ lolpebble.js
@@ -1,8 +1,16 @@
-simply.style('mono');
 var lolApiUrl = 'https://prod.api.pvp.net/api/lol/na/v1.1';
 var lolApiKey = 'api_key=1e0b2bdd-8bf5-43ba-8900-e7c606344517';
 
 var League = {};
+
+League.gameInfos = [
+    recentGameIndex,
+    gameType,
+    gameTime,
+    kda,
+    winOrLose,
+    champion
+];
 
 // Gets Player ID from Riot API.
 League.requestSummonerId = function(player, callback) {
