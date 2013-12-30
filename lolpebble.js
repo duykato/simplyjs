ef2122e8c5144e37c3313956d541cb63da014470
duykato <leduyq@gmail.com>
2013-12-29 23:23:21 -0800

    Update lolpebble.js

--- lolpebble.js
+++ lolpebble.js
@@ -45,3 +45,54 @@ for (i = 0 ; i < 4 ; i++){
         body: 'hello world.' + i
     }, true);    
 }
+
+// Displays recent game history information.
+var main = function() {
+  ajax({ url: lolApiUrl + '/game/by-summoner/21148858/recent?' + lolApiKey, type: 'json' }, function(data) {
+    var games = data.games;
+    for (var i = 0; i < games.length ; ++i) {
+      var game = games[i];
+      var gameType = game.subType;
+      if (game.subType == 'NORMAL') {		//Renames a game subtype.
+        gameType = 'Normal';
+      } else if (game.subType == 'RANKED_SOLO_5x5'){
+        gameType = 'Ranked Solo';      
+      } else if (game.subType == 'FIRSTBLOOD_1x1'){
+        gameType = 'Showdown 1v1';
+      } else if (game.subType == 'ARAM_UNRANKED_5x5'){
+        gameType = 'All Random All Mid';
+      }
+      var gameTime = moment(game.createDate).zone("-08:00").format('MM'+'/'+'DD'+'/'+'YY, h:mm:ss a');
+      var champion = League.getChampion(game.championId).name;     
+      var kills = 0, deaths = 0, assists = 0, winOrLose = 0;
+      for (var j = 0; j < game.statistics.length; ++j) {
+        var stat = game.statistics[j];
+        if (stat.name == 'CHAMPIONS_KILLED') {
+          kills = stat.value;
+        }
+        if (stat.name == 'DEATHS') {
+          deaths = stat.value;
+        }
+        if (stat.name == 'ASSISTS') {
+          assists = stat.value;
+        }
+        if (stat.name == 'WIN'){
+          var win = stat.value;        
+          if (win == 1){
+            winOrLose = 'Win';
+          }
+        }
+        if (stat.name == 'LOSE'){
+          var lose = stat.value;        
+          if (lose == 1){
+            winOrLose = 'Lose';
+          } 
+        }              
+      }
+      var kda = kills + '/' + deaths + '/' + assists;
+      simply.text({
+        body: gameType + '\n' + gameTime + '\n' + kda + '-' + winOrLose + '\n' + champion          
+      });
+    }
+  });
+};
