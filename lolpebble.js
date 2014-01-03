09c95771966b9e1c69ed60fe66d67f12de846b3b
duykato <leduyq@gmail.com>
2014-01-03 13:00:46 -0800

    Update lolpebble.js

--- lolpebble.js
+++ lolpebble.js
@@ -18,30 +18,12 @@ League.playerInfo = {
     season: 'Season3'
 };
 
-/*
-var changePlayer = function(delta) {
-  League.playerInfo.playerIndex += delta;
-  if (League.playerInfo.playerIndex < 0) {
-    League.playerInfo.playerIndex = League.playerInfo.players.length - 1;
-  } else if (League.playerInfo.playerIndex >= League.playerInfo.players.length) {
-    League.playerInfo.playerIndex = 0;
-  }
+var printPlayerStats = function(input) {
+    simply.text({
+        title: League.playerInfo.playerNames[input],
+        subtitle: 'yep',
+        body: '',
+    });
 };
 
-
-simply.on('singleClick', function(e) {
-  if (e.button === 'up') {
-    changePlayer(-1);
-    updatePlayer();
-  } else if (e.button === 'down') {
-    changePlayer(1);
-    updatePlayer();
-  }
-});
-*/
-
-for(var i = 0 ; i < League.playerInfo.playerNames.length ; ++i) {
-    simply.setText({
-        body: League.playerInfo.playerNames[i] + '\n' + '\n'
-    }, true);
-}
+printPlayerStats(0);
