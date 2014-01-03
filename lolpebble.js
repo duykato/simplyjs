44a575181e18824adcfedb207d33bc1ae4b40340
duykato <leduyq@gmail.com>
2014-01-03 13:17:59 -0800

    Update lolpebble.js

--- lolpebble.js
+++ lolpebble.js
@@ -1,28 +1,79 @@
-/* global simply*/
-simply.style('mono');
-var League = [];
+/*global ajax, simply*/
 
-// Object contains all player information along with a selection of names for scrolling through.
-League.playerInfo = {
-    playerNames: [
-        'duykato',
-        'pr0j3kt',
-        'lifeisgood',
-        'neoarchangel9',
-        'timolawl'
-    ],
-    players: [], // This is where all the player info will be stored. REMEMBER THIS SHIT. NO SERIOSLY REMEMBER IT.
-    playerIndex: 0,
-    summaryType: 'RankedSolo5x5',
-    season: 'Season3'
+var lolDuy = {
+  playerNames: [
+    'DuyKato',
+    'timolawl',
+    'neoarchangel9',
+    'lifeisgood',
+    'pr0j3kt',
+  ],
+  players: [],
+  playerIndex: 0,
+  summaryType: 'RankedSolo5x5',
+  season: 'SEASON3',
 };
 
-var printPlayerStats = function(input) {
-    simply.text({
-        title: League.playerInfo.playerNames[input],
-        subtitle: 'yep',
-        body: '',
-    });
+var initPlayers = function() {
+  var playerNames = lolDuy.playerNames;
+  for (var i = 0, ii = playerNames.length; i < ii; ++i) {
+    lolDuy.players[i] = {
+      name: playerNames[i],
+      id: '',
+      summary: { losses: 0, wins: 0 },
+    };
+  }
 };
 
-printPlayerStats(0);
+var lolApiUrl = 'https://prod.api.pvp.net/api/lol/na/v1.1';
+var lolApiKey = 'api_key=1e0b2bdd-8bf5-43ba-8900-e7c606344517';
+
+var requestSummonerId = function(player, callback) {
+  simply.setText({ title: player.name }, true);
+  var url = lolApiUrl+'/summoner/by-name/'+player.name+'?'+lolApiKey;
+  ajax({ url: url, type: 'json' }, function(data) {
+    player.id = data.id;
+    if (callback) { callback(player); }
+  });
+};
+
+var requestGameSummary = function(player) {
+  var url = lolApiUrl+'/stats/by-summoner/'+player.id+'/summary?season='+lolDuy.season+'&'+lolApiKey;
+  ajax({ url: url, type: 'json' }, function(data) {
+    var summaries = data.playerStatSummaries;
+    for (var i = 0, ii = summaries.length; i < ii; ++i) {
+      var summary = summaries[i];
+      if (summary.playerStatSummaryType === lolDuy.summaryType) {
+        player.summary = summary;
+      }
+    }
+    simply.setText({ body: player.summary.wins + ' / ' + player.summary.losses });
+  });
+};
+
+initPlayers();
+
+var updatePlayer = function() {
+  requestSummonerId(lolDuy.players[lolDuy.playerIndex], requestGameSummary);
+};
+
+var changePlayer = function(delta) {
+  lolDuy.playerIndex += delta;
+  if (lolDuy.playerIndex < 0) {
+    lolDuy.playerIndex = lolDuy.players.length - 1;
+  } else if (lolDuy.playerIndex >= lolDuy.players.length) {
+    lolDuy.playerIndex = 0;
+  }
+};
+
+simply.on('singleClick', function(e) {
+  if (e.button === 'up') {
+    changePlayer(-1);
+    updatePlayer();
+  } else if (e.button === 'down') {
+    changePlayer(1);
+    updatePlayer();
+  }
+});
+
+updatePlayer();
