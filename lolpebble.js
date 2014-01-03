19c30962bf306cfc8210da5d513932484dafdb47
duykato <leduyq@gmail.com>
2014-01-03 02:03:13 -0800

    Update lolpebble.js

--- lolpebble.js
+++ lolpebble.js
@@ -161,6 +161,7 @@ League.bindRecentGames = function(){
     });
 };
 
+// Retrieves last 10 matches from Riot API and loads it into an object named "recentGame".
 var main = function() {
   ajax({ url: lolApiUrl + '/game/by-summoner/' + League.requestSummonerId + '/recent?' + lolApiKey, type: 'json' }, function(data) {
     var games = data.games;
