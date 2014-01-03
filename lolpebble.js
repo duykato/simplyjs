9993c08c56d3f6b097d9e86dc18417c5a61864e0
duykato <leduyq@gmail.com>
2014-01-03 12:53:00 -0800

    Update lolpebble.js

--- lolpebble.js
+++ lolpebble.js
@@ -2,7 +2,6 @@
 
 var League = [];
 
-
 // Object contains all player information along with a selection of names
 // for scrolling through. Creates an array for player information to be stored.
 League.playerInfo = {
@@ -19,6 +18,30 @@ League.playerInfo = {
     season: 'Season3'
 };
 
-simply.setText({
-    body: League.playerInfo.playerNames[2]
+/*
+var changePlayer = function(delta) {
+  League.playerInfo.playerIndex += delta;
+  if (League.playerInfo.playerIndex < 0) {
+    League.playerInfo.playerIndex = League.playerInfo.players.length - 1;
+  } else if (League.playerInfo.playerIndex >= League.playerInfo.players.length) {
+    League.playerInfo.playerIndex = 0;
+  }
+};
+
+
+simply.on('singleClick', function(e) {
+  if (e.button === 'up') {
+    changePlayer(-1);
+    updatePlayer();
+  } else if (e.button === 'down') {
+    changePlayer(1);
+    updatePlayer();
+  }
 });
+*/
+
+for(var i = 0 ; i < League.playerInfo.playerNames.length ; ++i) {
+    simply.setText({
+        body: League.playerInfo.playerNames[i] + '\n'
+    });
+}
